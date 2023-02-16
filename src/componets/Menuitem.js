a929b0cfee3cf66655e8d8c115f4634a3b1aaa0e
Deepak Mishra <deepak@wisitech.com>
2023-02-16 17:48:42 +0530

    Update Menu Item

--- src/componets/Menuitem.js
+++ src/componets/Menuitem.js
@@ -1,23 +1,22 @@
-import { useState } from "react";
+import { useState } from "react"
 
 function Menuitem({item}){
     const [open, setOpen] = useState(false);
 
-    var iClass = item.level ==1 ? 'fa fa-angle-right' : 'fa fa-angle-down';
-    const liClass = item.level ==1 ? 'sub_menu_arrow' : 'menu_arrow';
-    var subMenuClass = item.level ==1 ? 'sub_sub_menu' : 'submenu';
-    var menuActive = item.level ==1 ? 'submenu_active' : 'menu_active';
+    var iClass = item.level ==1 ? 'fa fa-angle-right' : 'fa fa-angle-down'
+    const liClass = item.level ==1 ? 'sub_menu_arrow' : 'menu_arrow'
+    var subMenuClass = item.level ==1 ? 'sub_sub_menu' : 'submenu'
+    var menuActive = item.level ==1 ? 'submenu_active' : 'menu_active'
 
     if(open){
-        iClass = `${iClass} ${menuActive}`;
-        subMenuClass = `${subMenuClass} show`;
+        iClass = `${iClass} ${menuActive}`
+        subMenuClass = `${subMenuClass} show`
     }
         
     if(item.childrens){
         return (
             <li className={liClass}>
                 <a href={item.path || "#"}>{item.title}</a>
-                
                 <i className={iClass} onClick={() => setOpen(!open)} />
                 
                 <ul className={subMenuClass} style={{ display: open ? 'block' : '' }}>
